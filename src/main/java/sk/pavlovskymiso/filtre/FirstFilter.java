ac19827160529a9eef3db8416bdc713dbd242d55
Michal Pavlovsky <pavlovsky.miso@gmail.com>
2023-09-04 19:53:46 +0200

    firstFilter

---
+++ src/main/java/sk/pavlovskymiso/filtre/FirstFilter.java
@@ -0,0 +1,27 @@
+package sk.pavlovskymiso.filtre;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import java.io.IOException;
+
+
+@WebFilter(urlPatterns = "/*",
+        filterName = "FirstFilter name",
+        initParams = {@WebInitParam(name = "meno", value = "Michal Pavlovsky")})
+public class FirstFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
